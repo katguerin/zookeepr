119f689b996d158bf8503ae6220745d9d243fd04
katguerin <katguerin55@gmail.com>
2020-05-19 00:01:22 -0700

    at the end of module 11.1.7

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const { animals } = require('./data/animals');
 
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -51,6 +58,15 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!');
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    } 
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
