2921747c0389ce23b8fc4ca73e41c751363147e9
katguerin <katguerin55@gmail.com>
2020-05-23 16:15:57 -0700

    near the end of module 11.4. added files for zookeepers to work remotely

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const apiRoutes = require('./routes/apiRoutes');
+
+const htmlRoutes = require('./routes/htmlRoutes');
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -17,48 +21,10 @@ app.use(express.json());
 // to connect css and all other middleware files within the public folder
 app.use(express.static('public'));
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    } 
-});
-
-app.post('/api/animals', (req,res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
